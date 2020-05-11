665a8f1df2ef3e470fac07fa2a194bbd9cd47157
yangjaejae <undertakin@naver.com>
2020-05-11 11:25:59 +0900

    docker

--- start.sh
+++ start.sh
@@ -3,46 +3,16 @@
 DOCKER_CONTAINER_NAME="was"
 DOCKER_IMAGE_NAME="was/nodejs"
 DOCKER_IMAGE_TAG="v1.0.0"
-COMPOSE_FILE=docker-compose.yaml
 echo "start WAS Server"
 
-function restartContainers() {
-  docker rm -f $(docker ps -aqf name="${DOCKER_CONTAINER_NAME}")
-  docker-compose -f ${COMPOSE_FILE} up -d 2>&1
-}
-
-function rebuildDocker() {
-    echo "Stop and Remove current WAS Server docker container..."
-    docker rm -f $(docker ps -aqf name="${DOCKER_CONTAINER_NAME}")
-
-    if [[ "$(docker images -q ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} 2> /dev/null)" != "" ]]; then
-        echo "Remove WAS Server docker IMAGE!!.."
-        docker rmi -f ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
-    fi
-
-    echo "Build WAS Server image.."
-    docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} .
-    docker-compose -f ${COMPOSE_FILE} up -d 2>&1
-}
-
-
-function askProceed () {
-  read -p "Remove CA-Registry Server images and container to rebuild those items(Y) or just restart container(N) ? [Y/N] " ans
-  case "$ans" in
-    y|Y|"" )
-      echo "Remove and rebuild docker container of WAS Server"
-      rebuildDocker
-    ;;
-    n|N )
-      echo "Restart docker container of WAS Server..."
-      restartContainers
-    ;;
-    * )
-      echo "invalid response"
-      askProceed
-    ;;
-  esac
-}
-
-
-askProceed
+echo "Build image#############################################"
+docker rm -f $(docker ps -aqf name="${DOCKER_CONTAINER_NAME}")
+docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} .
+docker run -d \
+           -p "4000:4000" \
+           --name="${DOCKER_IMAGE_NAME}" \
+           -v ~/vpc_web/:/usr/src/app \
+           -w "/usr/src/app" \
+           --restart=on-failure \
+           ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} \
+           npm start
