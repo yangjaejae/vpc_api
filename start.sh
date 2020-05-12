e1cc892fd262cd4eaa79ac4d90629792b599893f
yangjaejae <undertakin@naver.com>
2020-05-12 18:04:01 +0900

    appspec

--- start.sh
+++ start.sh
@@ -1,18 +1,15 @@
-  
 #!/bin/bash
 DOCKER_CONTAINER_NAME="was"
 DOCKER_IMAGE_NAME="was/nodejs"
 DOCKER_IMAGE_TAG="v1.0.0"
 echo "start WAS Server"
 
-echo "Build image#############################################"
-docker rm -f $(docker ps -aqf name="${DOCKER_CONTAINER_NAME}")
-docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} .
+echo "Start container#############################################"
 docker run -d \
            -p "4000:4000" \
            --name="${DOCKER_CONTAINER_NAME}" \
-           -v ~/vpc_api/:/usr/src/app \
+           -v /home/ec2-user/vpc_web/:/usr/src/app \
            -w "/usr/src/app" \
            --restart=on-failure \
            ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} \
-           npm start
+           bash -c "npm install && npm start"
